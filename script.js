2adb3b2aabb2d28bd1f7cb1b4583af15496ae2ca
Antonio <tonito@umich.edu>
2021-11-07 11:52:44 -0500

    Final Result

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 var timeNote = ["8am", "9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm"]
-//An array of actual times?
 var times = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
 
-//Using Moment.js for current date and time
 var date = moment().format("LLLL")
 var currentTime = moment().hour();
 var currentDay = document.querySelector("#currentDay")
 
-//Put date on the screen
 currentDay.textContent = date
 
 for(i = 0; i < times.length; i++) {
     var taskTime = times[i]
-    //document.createElement("div")
     var row = document.createElement("div")
 
-    // Everything is showing past
     if(currentTime < taskTime){
         row.setAttribute("class", "row future")
     }
@@ -35,19 +30,15 @@ for(i = 0; i < times.length; i++) {
     var task = document.createElement("div")
     task.setAttribute("class", "col-10")
     row.appendChild(task)
+
     var save = document.createElement("div")
     save.setAttribute("class", "col-1 saveBtn")
     row.appendChild(save)
 
-    // Created an input element to create task 
-    // What CSS property will allow me to stretch this accross the task div
-    document.createElement('input')
-    var input = document.createElement('input')
+    var input = document.createElement('textarea')
     input.setAttribute("class", "textarea")
     task.appendChild(input)
-
-    // Created button within the save div but now it need to be bigger
-    // Needs to save the information 
+ 
     document.createElement("button")
     var button = document.createElement("button")
     button.setAttribute("class", "saveBtn")
@@ -56,11 +47,17 @@ for(i = 0; i < times.length; i++) {
     $(".container").append(row)
 
 }
-save.addEventListener("click", saveTask);
-console.log();
 
+var inputValue = document.querySelector(".textarea")
 
-//When do I save to local storage? When the save button is clicked
 var saveTask = function() {
-    localStorage.setItem("task", input);
+    localStorage.setItem(hour, inputValue.value);
+}
+
+var saveButton = document.querySelector(".saveBtn")
+saveButton.addEventListener("click", saveTask);
+
+for(i = 0; i < times.length; i++) {
+    var task = "task" + i
+    localStorage.getItem(task, inputValue.value)
 }
